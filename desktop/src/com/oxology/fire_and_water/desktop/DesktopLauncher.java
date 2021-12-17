a76ab81c1e8f99302fd710b3b17d0c4bfdea063e
Masiu121 <maks.kapolka@gmail.com>
2021-12-17 22:52:40 +0100

    Initial commit

---
+++ desktop/src/com/oxology/fire_and_water/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.oxology.fire_and_water.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.oxology.fire_and_water.FireAndWater;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FireAndWater(), config);
+	}
+}
