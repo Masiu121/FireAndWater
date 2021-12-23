993fdd4a0c21584324f4b09ea1613520b509892f
Masiu121 <maks.kapolka@gmail.com>
2021-12-23 16:47:17 +0100

    Updated FireAndWater

--- core/src/com/oxology/fire_and_water/FireAndWater.java
+++ core/src/com/oxology/fire_and_water/FireAndWater.java
@@ -4,15 +4,32 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.oxology.fire_and_water.net.GameClient;
+import com.oxology.fire_and_water.net.GameServer;
+
+import java.util.Scanner;
 
 public class FireAndWater extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	GameServer server;
+	GameClient client;
+	Scanner scanner = new Scanner(System.in);
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		System.out.print("Do you want to run the server? (y/n): ");
+		char choice = scanner.next().charAt(0);
+		if(choice == 'y') {
+			server = new GameServer(this);
+			server.start();
+		}
+
+		client = new GameClient(this, "localhost");
+		client.start();
+		client.sendData("ping".getBytes());
 	}
 
 	@Override
