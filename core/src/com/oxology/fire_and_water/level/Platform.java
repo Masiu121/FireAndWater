0d13fa4762e2dbcc62cabbbf74606a87b8dacfbd
Masiu121 <maks.kapolka@gmail.com>
2021-12-24 00:49:16 +0100

    Updated Platform

--- core/src/com/oxology/fire_and_water/level/Platform.java
+++ core/src/com/oxology/fire_and_water/level/Platform.java
@@ -3,16 +3,13 @@ package com.oxology.fire_and_water.level;
 import java.util.List;
 
 public class Platform {
-    float xOffset, yOffset;
-    List<LinearFunction> sides;
+    public float x, y;
+    public float width, height;
 
-    public Platform(float xOffset, float yOffset) {
-        this.xOffset = xOffset;
-        this.yOffset = yOffset;
-    }
-
-    public void addSide(int a, int x, int b) {
-        LinearFunction function = new LinearFunction(a, x, b);
-        sides.add(function);
+    public Platform(float x, float y, float width, float height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
     }
 }
