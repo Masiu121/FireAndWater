ae08d7508a91ae14438eed353921ec98cb149537
Masiu121 <maks.kapolka@gmail.com>
2021-12-24 16:48:50 +0100

    Updated Platform

--- core/src/com/oxology/fire_and_water/level/Platform.java
+++ core/src/com/oxology/fire_and_water/level/Platform.java
@@ -1,15 +1,17 @@
 package com.oxology.fire_and_water.level;
 
-import java.util.List;
+import com.badlogic.gdx.graphics.Texture;
 
 public class Platform {
     public float x, y;
     public float width, height;
+    public Texture texture;
 
-    public Platform(float x, float y, float width, float height) {
+    public Platform(float x, float y, float width, float height, Texture texture) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+        this.texture = texture;
     }
 }
