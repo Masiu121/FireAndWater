fc6c00fe24ff2769eeca744aaf23d761b43ed139
Masiu121 <maks.kapolka@gmail.com>
2021-12-24 16:48:44 +0100

    Updated Level

--- core/src/com/oxology/fire_and_water/level/Level.java
+++ core/src/com/oxology/fire_and_water/level/Level.java
@@ -1,13 +1,22 @@
 package com.oxology.fire_and_water.level;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Level {
     public List<Platform> platforms = new ArrayList<>();
 
-    public void addPlatform(float x, float y, float width, float height) {
-        Platform platform = new Platform(x, y, width, height);
+    public void addPlatform(float x, float y, float width, float height, Texture texture) {
+        Platform platform = new Platform(x, y, width, height, texture);
         platforms.add(platform);
+    }
+
+    public void drawLevel(SpriteBatch batch) {
+        for(Platform platform : platforms) {
+            batch.draw(platform.texture, platform.x, platform.y, platform.width, platform.height);
+        }
     }
 }
