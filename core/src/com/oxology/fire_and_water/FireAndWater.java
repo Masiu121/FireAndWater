fa98fabd530b6644606d806297b6e620f22baa03
Masiu121 <maks.kapolka@gmail.com>
2021-12-24 16:48:38 +0100

    Updated FireAndWater

--- core/src/com/oxology/fire_and_water/FireAndWater.java
+++ core/src/com/oxology/fire_and_water/FireAndWater.java
@@ -2,6 +2,8 @@ package com.oxology.fire_and_water;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
@@ -25,15 +27,31 @@ public class FireAndWater extends ApplicationAdapter {
 	BitmapFont font;
 	GlyphLayout layout;
 	public Level level;
+	boolean showInfo;
 	
 	@Override
 	public void create () {
+		showInfo = false;
+		img = new Texture("badlogic.jpg");
+
 		level = new Level();
-		level.addPlatform(100f, 300f, 300, 50);
-		level.addPlatform(300f, 290f, 200, 50);
+		//floor
+		level.addPlatform(0f, -50f, 1920f, 100f, img);
+
+		//stairs
+		level.addPlatform(1000f, 100f, 400f, 50f, img);
+		level.addPlatform(1350f, 200f, 400f, 50f, img);
+		level.addPlatform(1700f, 300f, 400f, 50f, img);
+		level.addPlatform(1750f, 400f, 400f, 50f, img);
+		level.addPlatform(1800f, 500f, 400f, 50f, img);
+
+		//platforms
+		level.addPlatform(900f, 550f, 500f, 50f, img);
+		level.addPlatform(200f, 350f, 500f, 50f, img);
 
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		/*
 		System.out.print("Do you want to run the server? (y/n): ");
 		char choice = scanner.next().charAt(0);
 		if(choice == 'y') {
@@ -44,6 +62,7 @@ public class FireAndWater extends ApplicationAdapter {
 		client = new GameClient(this, "localhost");
 		client.start();
 		client.sendData("ping".getBytes());
+		*/
 
 		font = new BitmapFont(Gdx.files.internal("PixelFont.fnt"));
 		font.getData().scale(0.5f);
@@ -63,11 +82,32 @@ public class FireAndWater extends ApplicationAdapter {
 	@Override
 	public void render () {
 		update();
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 1, 1);
+		if(Gdx.input.isKeyJustPressed(Input.Keys.F3))
+			toggleDebug();
 		batch.begin();
 		player.draw(batch, font);
-		batch.draw(img, level.platforms.get(0).x, level.platforms.get(0).y, level.platforms.get(0).width, level.platforms.get(0).height);
-		batch.draw(img, level.platforms.get(1).x, level.platforms.get(1).y, level.platforms.get(1).width, level.platforms.get(1).height);
+		level.drawLevel(batch);
+		if(showInfo) {
+			int lineHeight = 20;
+			int lineOffset = -5;
+			font.setColor(Color.WHITE);
+			font.getData().setScale(0.4f);
+
+			font.draw(batch, "Collision TOP: ", 2, Gdx.graphics.getHeight() - lineHeight * 0 + lineOffset);
+			font.draw(batch, "Collision DOWN: ", 2, Gdx.graphics.getHeight() - lineHeight * 1 + lineOffset);
+			font.draw(batch, "Collision RIGHT: ", 2, Gdx.graphics.getHeight() - lineHeight * 2 + lineOffset);
+			font.draw(batch, "Collision LEFT: ", 2, Gdx.graphics.getHeight() - lineHeight * 3 + lineOffset);
+			font.draw(batch, "XY: " + player.x + ", " + player.y, 2, Gdx.graphics.getHeight() - lineHeight * 4 + lineOffset);
+
+			for (int i = 0; i < 4; i++) {
+				if (player.colliders[i] != null) {
+					font.draw(batch, "True", 220, Gdx.graphics.getHeight() - lineHeight * (i) + lineOffset);
+				} else {
+					font.draw(batch, "False", 220, Gdx.graphics.getHeight() - lineHeight * (i) + lineOffset);
+				}
+			}
+		}
 		batch.end();
 	}
 
@@ -79,5 +119,9 @@ public class FireAndWater extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+	}
+
+	public void toggleDebug() {
+		showInfo = !showInfo;
 	}
 }
