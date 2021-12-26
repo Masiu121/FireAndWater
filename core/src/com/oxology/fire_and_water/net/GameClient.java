83f9933db7b16b8e06a1fe8984c959a64effc146
Masiu121 <maks.kapolka@gmail.com>
2021-12-27 00:08:41 +0100

    Updated GameClient

--- core/src/com/oxology/fire_and_water/net/GameClient.java
+++ core/src/com/oxology/fire_and_water/net/GameClient.java
@@ -1,6 +1,10 @@
 package com.oxology.fire_and_water.net;
 
 import com.oxology.fire_and_water.FireAndWater;
+import com.oxology.fire_and_water.net.packets.Packet;
+import com.oxology.fire_and_water.net.packets.Packet.PacketType;
+import com.oxology.fire_and_water.net.packets.Packet00;
+import com.oxology.fire_and_water.net.packets.Packet01;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
@@ -31,8 +35,26 @@ public class GameClient extends Thread {
             } catch(IOException e) {
                 e.printStackTrace();
             }
-            String message = new String(packet.getData()).trim();
-            System.out.println("[SERVER->CLIENT]: " + message);
+            parsePacket(packet);
+        }
+    }
+
+    public void parsePacket(DatagramPacket packet) {
+        Packet p = null;
+        String message = new String(packet.getData()).trim();
+        PacketType type = Packet.findPacket(message.substring(0, 2));
+        switch(type.packetType) {
+            case 00:
+                p = new Packet00(packet.getData());
+                System.out.println(((Packet00) p).getUsername() + " has joined the game");
+                PlayerMP playerMP = new PlayerMP(game, ((Packet00) p).getUsername(), ((Packet00) p).getX(), ((Packet00) p).getY(), packet.getAddress(), packet.getPort());
+                game.level.addEntity(playerMP);
+                break;
+            case 01:
+                p = new Packet01(packet.getData());
+                System.out.println(((Packet01) p).getUsername() + " has left the game");
+                game.level.removePlayer(((Packet01) p).getUsername());
+                break;
         }
     }
 
